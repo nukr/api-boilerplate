b7834e13d00636813acde34a9c00c9ff7b32afe9
Nukr <nukrs.w@gmail.com>
2016-03-16 19:09:31 +0800

    modify

--- src/app/index.js
+++ src/app/index.js
@@ -1,3 +1,19 @@
-;(async () => {
-  console.log('hihi')
-})()
+import Koa from 'koa'
+import Router from 'koa-router'
+
+const app = new Koa()
+const router = new Router()
+
+router.get('/', (ctx, next) => {
+  ctx.body = process.env.HOSTNAME
+})
+
+router.get('/healthCheck', (ctx, next) => {
+  ctx.set('Cache-Control', 'no-cache')
+  ctx.status = 200
+})
+
+app.use(router.routes())
+app.listen(3000, () => {
+  console.log('server listen on port 3000')
+})
